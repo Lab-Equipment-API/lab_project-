11d43a5c0c3edf4bfb79d1a0abaa21f70e6b2c9b
marthahab <marthahabtamu264@gmail.com>
2026-05-30 23:43:38 +0300

    Add Rule 4 - equipment name must be real validation

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -20,6 +20,18 @@ class Equipment < ApplicationRecord
               ]
             }
 
+ validate :name_must_be_real
 
+private
 
+def name_must_be_real
+  return if name.blank?
+  if name.length < 3
+    errors.add(:name, "must be at least 3 characters")
+  elsif !name.match?(/[a-zA-Z]/)
+    errors.add(:name, "must contain at least one letter")
+  end
 end
+end
+
+
